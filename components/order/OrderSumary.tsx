0e55978c60306038618df781f515521593f1dd37
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-08-08 12:50:52 -0600

    create(order): Crear componente para mostrar el resumen del pedido

---
+++ components/order/OrderSumary.tsx
@@ -0,0 +1,7 @@
+export default function OrderSumary() {
+    return (
+        <aside className="h-screen overflow-y-scroll w-64 p-5">
+            <h1 className="text-4xl text-center font-black">Mi pedido</h1>
+        </aside>
+    );
+};
