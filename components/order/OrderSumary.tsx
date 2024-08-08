c2cf50d1d2058012cf362e6fedc3c147d1dc270d
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-08-08 12:56:33 -0600

    Chore: Delete componente no usado

--- components/order/OrderSumary.tsx
+++
@@ -1,7 +0,0 @@
-export default function OrderSumary() {
-    return (
-        <aside className="h-screen overflow-y-scroll w-64 p-5">
-            <h1 className="text-4xl text-center font-black">Mi pedido</h1>
-        </aside>
-    );
-};
