80992cb51e32e72437c38c74b1913f9fc7401c7d
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-08-12 12:08:43 -0600

    Feat(order): Se muestran los iconos de cada categoria

--- components/order/OrderSideBar.tsx
+++ components/order/OrderSideBar.tsx
@@ -1,7 +1,23 @@
-export default function OrderSideBar() {
+import { prisma } from "@/src/lib/prisma";
+import CategoryIcon from "./ui/CategoryIcon";
+
+async function getCaregories() {
+    return await prisma.category.findMany()
+}
+
+export default async function OrderSideBar() {
+    const categories = await getCaregories()
+
     return (
-        <aside className="bg-white h-screen w-72">
-            OrderSideBar
+        <aside className="bg-white h-screen w-16 sm:w-60">
+            <nav className="mt-10">
+                {categories.map(category => (
+                    <CategoryIcon
+                        key={category.id}
+                        category={category}
+                    />
+                ))}
+            </nav>
         </aside>
     );
 };
