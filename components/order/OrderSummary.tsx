106ca74c99370c71c649e1201ed30cfc6387a849
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-08-16 13:22:52 -0600

    feat: Usamos el store para mostrar los productos dentro de la orden

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,7 +1,34 @@
+'use client'
+import { useStore } from "@/src/store";
+import ProductDetails from "./ProductDetails";
+import { useMemo } from "react";
+import { formatCurrency } from "@/src/utils";
+
 export default function OrderSumary() {
+
+    const { order } = useStore()
+    const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+
     return (
         <aside className="h-screen overflow-y-scroll w-64 p-5">
             <h1 className="text-4xl text-center font-black">Mi pedido</h1>
+
+            {order.length === 0 ? <p className="text-center my-10">El carrito está vacío</p> :
+                (
+                    <div className="mt-5">
+                        {order.map(item => (
+                            <ProductDetails
+                                key={item.id}
+                                product={item}
+                            />
+                        ))}
+
+                        <p className="text-2xl mt-20 text-center">
+                            Total a pagar: {''}
+                            <span className="font-bold ">{formatCurrency(total)}</span>
+                        </p>
+                    </div>
+                )}
         </aside>
     );
 };
