a6f7171c9dd70ac1f712de04a07f01fe9fda2898
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-08-21 15:54:18 -0600

    Feat: Se crea la action para almacenar ordenes

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -3,17 +3,47 @@ import { useStore } from "@/src/store";
 import ProductDetails from "./ProductDetails";
 import { useMemo } from "react";
 import { formatCurrency } from "@/src/utils";
+import { createOrder } from "@/actions/create-order-action";
+import { OrderSchema } from "@/src/schema";
+import { toast } from "react-toastify";
 
 export default function OrderSumary() {
 
-    const { order } = useStore()
+    const { order, clearOrder } = useStore()
     const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+
+    const handleCreateOrder = async (formData: FormData) => {
+        const data = {
+            name: formData.get('name'),
+            total,
+            order
+        }
+
+        const result = OrderSchema.safeParse(data)
+        if (!result.success) {
+            result.error.issues.forEach((issue) => {
+                toast.error(issue.message)
+            })
+
+            return
+        }
+
+        const response = await createOrder(data)
+        if (response?.errors) {
+            response.errors.forEach((issue) => {
+                toast.error(issue.message)
+            })
+        }
+
+        toast.success('Pedido realizado')
+        clearOrder()
+    }
 
     return (
         <aside className="h-screen overflow-y-scroll w-64 p-5">
             <h1 className="text-4xl text-center font-black">Mi pedido</h1>
 
-            {order.length === 0 ? <p className="text-center my-10">El carrito está vacío</p> :
+            {order.length === 0 ? <p className="text-center my-10">El pedido está vacío</p> :
                 (
                     <div className="mt-5">
                         {order.map(item => (
@@ -27,6 +57,24 @@ export default function OrderSumary() {
                             Total a pagar: {''}
                             <span className="font-bold ">{formatCurrency(total)}</span>
                         </p>
+
+                        <form
+                            className="w-full mt-10 space-y-5"
+                            action={handleCreateOrder}
+                        >
+
+                            <input
+                                type="text"
+                                placeholder="Tu nombre"
+                                className="bg-white border border-gray-100 p-2 w-full"
+                                name="name"
+                            />
+
+                            <input
+                                type="submit"
+                                className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
+                                value="Confirmar pedido" />
+                        </form>
                     </div>
                 )}
         </aside>
