5f5350257eac56c4d96830abdcc60b154e016a0b
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-08-26 11:18:34 -0600

    Feat: Se agrega el logotipo

--- components/order/OrderSideBar.tsx
+++ components/order/OrderSideBar.tsx
@@ -1,5 +1,6 @@
 import { prisma } from "@/src/lib/prisma";
 import CategoryIcon from "../ui/CategoryIcon";
+import Logo from "../ui/Logo";
 
 async function getCaregories() {
     return await prisma.category.findMany()
@@ -10,6 +11,7 @@ export default async function OrderSideBar() {
 
     return (
         <aside className="bg-white h-screen w-16 sm:w-60">
+            <Logo />
             <nav className="mt-10">
                 {categories.map(category => (
                     <CategoryIcon
