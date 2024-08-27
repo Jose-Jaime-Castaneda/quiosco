3e7144c96d56807a5c0743b2e66eb9b7ad62ab5a
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-08-27 09:27:54 -0600

    Feat: Se crea el componente para mostrar las opciones del panel de administrador

--- components/admin/AdminSidebar.tsx
+++ components/admin/AdminSidebar.tsx
@@ -1,7 +1,9 @@
+import AdminRoute from "./AdminRoute"
+
 const adminNavigation = [
-    {url: '/admin/orders', text: 'Ordenes', blank: false},
-    {url: '/admin/products', text: 'Productos', blank: false},
-    {url: '/order/cafe', text: 'Ver Quiosco', blank: true},
+    { url: '/admin/orders', text: 'Ordenes', blank: false },
+    { url: '/admin/products', text: 'Productos', blank: false },
+    { url: '/order/cafe', text: 'Ver Quiosco', blank: true },
 ]
 
 export default function AdminSidebar() {
@@ -12,7 +14,12 @@ export default function AdminSidebar() {
             <div className="space-y-3 ">
                 <p className="mt-10 uppercase font-bold text-sm text-gray-600 text-center">Navegación</p>
                 <nav className="flex flex-col">
-                    
+                    {adminNavigation.map(link => (
+                        <AdminRoute
+                            key={link.url}
+                            link={link}
+                        />
+                    ))}
                 </nav>
             </div>
         </>
