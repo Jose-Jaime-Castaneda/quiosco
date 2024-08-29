c8c5ee999e7c68e4690642d620958ad1316058e1
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-08-29 14:55:52 -0600

    Feat: Se agrega el schema para validar el formualrio de búsqueda

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -2,12 +2,21 @@ import { z } from "zod";
 
 export const OrderSchema = z.object({
   name: z.string().min(1, "Tu nombre es obligatorio"),
-  total: z.number().min(1, 'Hay errores en la orden'),
-  order: z.array(z.object({
-    id: z.number(),
-    name: z.string(),
-    price: z.number(),
-    quantity: z.number(),
-    subtotal: z.number()
-  }))
+  total: z.number().min(1, "Hay errores en la orden"),
+  order: z.array(
+    z.object({
+      id: z.number(),
+      name: z.string(),
+      price: z.number(),
+      quantity: z.number(),
+      subtotal: z.number(),
+    })
+  ),
+});
+
+export const SearchShema = z.object({
+  search: z
+    .string()
+    .trim()
+    .min(1, { message: "La busqueda no puede ir vacia" }),
 });
