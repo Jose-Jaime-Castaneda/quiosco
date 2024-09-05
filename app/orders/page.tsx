c5093e11333acb79628fc1adbc1d73dc664f41a2
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-09-05 12:17:47 -0600

    Feat: Se agrega SWT y la vista de ordenes realizadas

---
+++ app/orders/page.tsx
@@ -0,0 +1,74 @@
+
+//import useSWR from "swr";
+import LatestOrderItem from "@/components/order/LatestOrderItem";
+import Logo from "@/components/ui/Logo";
+import { prisma } from "@/src/lib/prisma";
+import { revalidatePath } from "next/cache";
+
+async function getOrdersReady() {
+    const orders = await prisma.order.findMany({
+        take: 5,
+        where: {
+            orderReadyAt: {
+                not: null
+            }
+        },
+        orderBy: {
+            orderReadyAt: 'desc'
+        },
+        include: {
+            orderProducts: {
+                include: {
+                    product: true
+                }
+            }
+        }
+    })
+
+    return orders
+}
+
+export default async function page() {
+    /** For SWR 
+     * 
+     *  const url = '/orders/api'
+     *  const fetcher = fetch(url).then(res => res.json()).then(data => data)
+     *  const {data, error, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+     *      refreshInterval: 60000,
+     *      revalidateOnFocus: false,
+     *  })
+     */
+
+    const orders = await getOrdersReady()
+
+    const handleSubmit = async () => {
+        "use server"
+        revalidatePath('/orders')
+    }
+
+    return (
+        <>
+            <h1 className="text-center mt-20 text-6xl font-black">Ordenes Listas</h1>
+
+            <Logo />
+
+            <form action={handleSubmit}>
+                <input
+                    type="submit"
+                    className="bg-amber-400 w-full lg:w-auto text-xl px-3 py-3 text-center font-bold cursor-pointer"
+                    value="Actualizar Ordenes" />
+            </form>
+
+            {orders.length ? (
+                <div className="grid grid-cols-2 gap-5 max-w-5xl mx-auto mt-10">
+                    {orders.map(order => (
+                        <LatestOrderItem
+                            key={order.id}
+                            order={order}
+                        />
+                    ))}
+                </div>
+            ) : <p className='text-center my-10'>No hay ordenes listas</p>}
+        </>
+    );
+};
